59b6de0b86d0d8b816c9b81cae61d6059b12ee23
Manish Kumar Naik <165175167+devmanishkumarnaik@users.noreply.github.com>
2025-11-29 12:22:12 +0530

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,9 +1,25 @@
+// import mongoose from "mongoose";
+
+// const productSchema = new mongoose.Schema({
+//   sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "Seller", required: true },
+//   firstName: { type: String, required: true },
+//   lastName: { type: String, required: true },
+//   productName: { type: String, required: true },
+//   details: { type: String, required: false, default: "" },
+//   imageUrl: { type: String, required: true },
+//   location: { type: String, required: true },
+//   code: { type: String, required: true, unique: true }, // 5-character alphanumeric code
+//   available: { type: Boolean, default: true },
+//   createdAt: { type: Date, default: Date.now }
+// });
+
+// export default mongoose.model("Product", productSchema);
+
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
   sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "Seller", required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  name: { type: String, required: true },
   productName: { type: String, required: true },
   details: { type: String, required: false, default: "" },
   imageUrl: { type: String, required: true },
