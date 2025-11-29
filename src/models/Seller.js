8640cc90406d8c8c44a7a2729ba5148adf5bfc77
Manish Kumar Naik <165175167+devmanishkumarnaik@users.noreply.github.com>
2025-11-29 12:35:58 +0530

    Update Seller.js

--- src/models/Seller.js
+++ src/models/Seller.js
@@ -1,20 +1,8 @@
-// import mongoose from "mongoose";
-
-// const sellerSchema = new mongoose.Schema({
-//   firstName: { type: String, required: true },
-//   lastName: { type: String, required: true },
-//   email: { type: String, required: true, unique: true, lowercase: true },
-//   password: { type: String, required: true },
-//   phone: { type: String, required: true },
-//   createdAt: { type: Date, default: Date.now }
-// });
-
-// export default mongoose.model("Seller", sellerSchema);
-
 import mongoose from "mongoose";
 
 const sellerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
   phone: { type: String, required: true },
